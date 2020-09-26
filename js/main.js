0f61d41ec575f664249cc48d7c91c235172391ff
Misha-13 <m.a.cherkasov@yandex.ru>
2020-09-26 20:08:29 +0300

    Добавляет отображение пинов

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
 'use strict';
-let REALTY_TYPES = ['palace', 'flat', 'house', 'bungalow'];
-let TIME_BOUNDS = ['12:00', '13:00', '14:00'];
-let FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-let PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+let REALTY_TYPES = [`palace`, `flat`, `house`, `bungalow`];
+let TIME_BOUNDS = [`12:00`, `13:00`, `14:00`];
+let FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+let PHOTOS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
 let MIN_X = 10;
 let MAX_X = 765;
 let MIN_Y = 130;
 let MAX_Y = 630;
+let MIN_PRICE = 500;
+let MAX_PRICE = 500000;
+let MIN_ROOMS = 1;
+let MAX_ROOMS = 5;
+let MIN_GUESTS = 1;
+let MAX_GUESTS = 10;
+let X_SHIFT = 25;
+let Y_SHIFT = 70;
+let map = document.querySelector(`.map`);
+let pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+let pinsList = document.querySelector(`.map__pins`);
+let fragment = document.createDocumentFragment();
 
-let generateIndices = function (iteration) {
+let generateIndices = function (iteration, exitValue = 1) {
   let indices = [];
   while (indices.length < iteration) {
     let randomIndex = Math.round(Math.random() * iteration);
     if (!indices.includes(randomIndex) && randomIndex !== 0) {
       indices.push(randomIndex);
+    }
+    if (Math.random() > exitValue) {
+      return indices;
     }
   }
   return indices;
@@ -23,47 +39,81 @@ let generateAvatarsArr = function (iteration = 8) {
   let avatars = [];
   let imgIndices = generateIndices(iteration);
   for (let i = 0; i < iteration; i++) {
-    avatars.push('img/avatars/user0' + imgIndices[i] + '.png');
+    avatars.push(`img/avatars/user0` + imgIndices[i] + `.png`);
   }
   return avatars;
 };
 
-var getRandomElement = function (currentArray) {
-  var maxIndex = currentArray.length - 1;
+let getRandomElement = function (currentArray) {
+  let maxIndex = currentArray.length - 1;
   return Math.round(Math.random() * maxIndex);
 };
 
-var getElementByRandomIndex = function (currentArray) {
-  var index = getRandomElement(currentArray);
+let getElementByRandomIndex = function (currentArray) {
+  let index = getRandomElement(currentArray);
   return currentArray[index];
 };
 
-let createTopics = function (elementQuantity = 8) {
-  let topics = [];
+let createValueInRange = function (minBound, maxBound) {
+  let coordinate = Math.round(Math.random() * (maxBound - minBound) + minBound);
+  return coordinate;
+};
+
+let generateRandomLenghtArr = function (currentArray) {
+  let randomArr = [];
+  let randomArrLength = getRandomElement(currentArray);
+  for (let i = 0; i <= randomArrLength; i++) {
+    randomArr.push(currentArray[i]);
+  }
+  return randomArr;
+};
+
+let createPins = function (elementQuantity = 8) {
+  let pins = [];
   let avatars = generateAvatarsArr();
   for (let i = 0; i < elementQuantity; i++) {
-    topics.push({
+    let x = createValueInRange(MIN_X, MAX_X);
+    let y = createValueInRange(MIN_Y, MAX_Y);
+    pins.push({
       'author': {
         'avatar': avatars[i]
       },
       'offer': {
-        'title': строка, заголовок предложения
-        'address': строка, адрес предложения. Для простоты пусть пока представляет собой запись вида '{{location.x}}, {{location.y}}', например, '600, 350'
-        'price': число, стоимость
-        'type': строка с одним из четырёх фиксированных значений: palace, flat, house или bungalow
-        'rooms': число, количество комнат
-        'guests': число, количество гостей, которое можно разместить
-        'checkin': строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
-        'checkout': строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
-        'features': массив строк случайной длины из ниже предложенных: 'wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner',
-        'description': строка с описанием,
-        'photos': массив строк случайной длины, содержащий адреса фотографий 'http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+        'title': `Тестовая строка заголовка ` + i,
+        'address': x + `, ` + y,
+        'price': createValueInRange(MIN_PRICE, MAX_PRICE),
+        'type': getElementByRandomIndex(REALTY_TYPES),
+        'rooms': createValueInRange(MIN_ROOMS, MAX_ROOMS),
+        'guests': createValueInRange(MIN_GUESTS, MAX_GUESTS),
+        'checkin': getElementByRandomIndex(TIME_BOUNDS),
+        'checkout': getElementByRandomIndex(TIME_BOUNDS),
+        'features': generateRandomLenghtArr(FEATURES),
+        'description': `Тестовое описание ` + i,
+        'photos': generateRandomLenghtArr(PHOTOS)
       },
       'location': {
-        'x': случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка. 788
-        'y': случайное число, координата y метки на карте от 130 до 630.
+        'x': x,
+        'y': y,
       }
     });
   }
-  return topics;
+  return pins;
 };
+
+let fillPins = function (pin) {
+  let pinElement = pinTemplate.cloneNode(true);
+  let img = pinElement.querySelector(`img`);
+  let pinX = pin.location.x - X_SHIFT;
+  let pinY = pin.location.y - Y_SHIFT;
+  pinElement.setAttribute(`style`, `left: ` + pinX + `px; top: ` + pinY + `px;`);
+  img.setAttribute(`src`, pin.author.avatar);
+  img.setAttribute(`alt`, pin.offer.title);
+  return pinElement;
+};
+
+let mapPins = createPins();
+map.classList.remove(`map--faded`);
+for (let i = 0; i < mapPins.length; i++) {
+  fragment.appendChild(fillPins(mapPins[i]));
+}
+pinsList.appendChild(fragment);
