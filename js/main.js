48e1fe0990550a8f8beb92ad198eb666ddd588a7
Misha-13 <m.a.cherkasov@yandex.ru>
2020-10-01 22:08:32 +0300

    Добавляет тип жилья на русском

--- js/main.js
+++ js/main.js
@@ -129,6 +129,12 @@ const fillCards = function (card) {
   const imgList = cardElement.querySelector(`.popup__photos`);
   const imgTemplate = cardElement.querySelector(`.popup__photos`).querySelector(`img`);
   const imgFragment = document.createDocumentFragment();
+  const typeRusMatch = {
+    palace: `Дворец`,
+    flat: `Квартира`,
+    house: `Дом`,
+    bungalow: `Бунгало`
+  };
 
   const getElementsAreNotInArray = function (currentArray) {
     let elementsForDelete = [];
@@ -156,9 +162,9 @@ const fillCards = function (card) {
   cardElement.querySelector(`.popup__title`).textContent = card.offer.title;
   cardElement.querySelector(`.popup__text--address`).textContent = card.offer.address;
   cardElement.querySelector(`.popup__text--price`).textContent = card.offer.price + `₽/ночь`;
-  cardElement.querySelector(`.popup__type`).textContent = card.offer.type;
+  cardElement.querySelector(`.popup__type`).textContent = typeRusMatch[card.offer.type];
   cardElement.querySelector(`.popup__text--capacity`).textContent = card.offer.rooms + ` комнаты для ` + card.offer.guests + ` гостей`;
-  cardElement.querySelector(`.popup__type`).textContent = `Заезд после ` + card.offer.checkin + `, выезд до ` + card.offer.checkout;
+  cardElement.querySelector(`.popup__text--time`).textContent = `Заезд после ` + card.offer.checkin + `, выезд до ` + card.offer.checkout;
   let delArr = getElementsAreNotInArray(card.offer.features);
   for (let i = 0; i < delArr.length; i++) {
     delArr[i].remove();
